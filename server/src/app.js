66ca3233e010066a8f3054dd276ec61e91ba45ef
yajurshi <yajurshivelani@gmail.com>
2026-07-12 12:24:38 +0530

    Backend setup and server intialization

---
+++ server/src/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "TransitOps Backend Running"
+    });
+});
+
+module.exports = app;
